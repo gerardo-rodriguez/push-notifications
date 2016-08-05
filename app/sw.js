f20c49e467c6b19af24adaa4a13b37f0da0186d8
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2016-08-05 14:04:11 -0700

    Final working state for sw

--- app/sw.js
+++ app/sw.js
@@ -20,5 +20,35 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('push', function(event) {
-  console.log('Push message received', event);
+  console.log('Push message', event);
+  var title = 'Push message';
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: 'The Message',
+      icon: 'images/icon.png',
+      tag: 'my-tag'
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('Notification click: tag', event.notification.tag);
+  event.notification.close();
+  var url = 'https://www.youtube.com/watch?v=L2e5aboF_ss';
+  event.waitUntil(
+    clients.matchAll({
+      type: 'window'
+    })
+    .then(function(windowClients) {
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        if (client.url === url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
 });
